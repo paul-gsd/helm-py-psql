bf0cd5102b4582ba7813be4fbd12f372edf6395d
Paul Klym <paul.klymenko@apolo.us>
2024-10-25 12:52:00 +0200

    Add psql test queries

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.12.5-slim-bullseye
 
 WORKDIR /app
 
+RUN pip install psycopg2
+
 COPY src/main.py .
 
 EXPOSE 8080
--- src/main.py
+++ src/main.py
@@ -1,4 +1,38 @@
 import os
+import psycopg2
 
 for key, value in os.environ.items():
     print(f"{key}: {value}")
+
+
+conn = psycopg2.connect(
+    dbname=os.getenv("PSQL_DATABASE"),
+    user=os.getenv("PSQL_USERNAME"),
+    password=os.getenv("PSQL_PASSWORD"),
+    host=os.getenv("PSQL_HOST"),
+    port=os.getenv("PSQL_PORT"),
+)
+
+cur = conn.cursor()
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS sample_table (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100)
+    )
+""")
+
+cur.execute("INSERT INTO sample_table (name) VALUES ('Row1')")
+cur.execute("INSERT INTO sample_table (name) VALUES ('Row2')")
+cur.execute("INSERT INTO sample_table (name) VALUES ('Row3')")
+
+conn.commit()
+
+cur.execute("SELECT * FROM sample_table")
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+conn.close()
